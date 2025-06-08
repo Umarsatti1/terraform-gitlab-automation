039b2441b79d4262925e8719a5d4a01b44b29bf8
Umarsatti1 <umarsatti.15@gmail.com>
2025-06-08 18:28:33 +0000

    Edit outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -9,11 +9,3 @@ output "public_dns_name" {
 output "private_ip_address" {
     value = aws_instance.my_ec2_instance[*].private_ip
 }
-
-output "s3_bucket_name" {
-    value = aws_s3_bucket.remote_s3.bucket
-}
-
-output "dynamodb_table_name" {
-    value = aws_dynamodb_table.terraform_state_lock.name
-}
